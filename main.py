7f5ac795b7f439efc69a75531749c3e5a2fda93c
Egor Digitax <egordigitax@digitax.local>
2023-04-02 14:05:12 +0500

    ready-to-go

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 import random
 from io import BytesIO
 from pathlib import Path
-
+import uuid
 import requests as requests
-from PIL import Image, PngImagePlugin
+from PIL import Image
 import os
 
 
@@ -47,37 +47,80 @@ class NFTImage:
 
     def generate_with_ai(self):
         image = self.generate()
-        img_base64 = self.encode_pil_to_base64(image)
-        with open('payload.json', "rb") as json_payload:
-            payload = json.load(json_payload)
-        payload['init_images'] = [img_base64]
-        payload['prompt'] = "anime girl"
-        print(json.dumps(payload))
-        r = requests.request("POST", 'https://9317831c-08c0-41b4.gradio.live/sdapi/v1/img2img',
+        img_base64 = self._image_to_base64(image)
+        payload = StyleConfig.octane_anime_girls(img_base64)
+        r = requests.request("POST", 'http://192.168.1.3:7855/sdapi/v1/img2img',
                              data=json.dumps(payload))
-        print(r)
-        return r.json()
+        base64_generated_image = r.json()['images'][0]
+        base64_upscaled_image = self.upscale_image(base64_generated_image).json()['image']
+        self._base64_to_image(base64_upscaled_image)
 
     @staticmethod
-    def encode_pil_to_base64(image):
-        with BytesIO() as output_bytes:
+    def _image_to_base64(image):
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        img_bytes = base64.b64encode(buffered.getvalue())
+        return img_bytes.decode('utf-8')
 
-            if True:
-                use_metadata = False
-                metadata = PngImagePlugin.PngInfo()
-                for key, value in image.info.items():
-                    if isinstance(key, str) and isinstance(value, str):
-                        metadata.add_text(key, value)
-                        use_metadata = True
-                image.save(output_bytes, format="PNG", pnginfo=(metadata if use_metadata else None))
+    @staticmethod
+    def _base64_to_image(b64):
+        img = Image.open(BytesIO(base64.b64decode(b64)))
+        img.save(f'results/{uuid.uuid4()}.png')
 
-            bytes_data = output_bytes.getvalue()
+    @staticmethod
+    def upscale_image(image):
+        payload = {}
+        payload['image'] = image
+        payload['upscaler_1'] = 'R-ESRGAN 4x+ Anime6B'
+        payload['upscaling_resize'] = 4
+        r = requests.request("POST", 'http://192.168.1.3:7855/sdapi/v1/extra-single-image',
+                             data=json.dumps(payload))
+        return r
 
-        return base64.b64encode(bytes_data).decode('utf-8')
+
+class StyleConfig:
+
+    @staticmethod
+    def octane_anime_girls(image):
+        """
+        USE DELIBERATEv2
+        :param image:
+        :return:
+        """
+        payload = {}
+        payload['init_images'] = [image]
+        payload['prompt'] = "anime girl, concept art, professional, ((soft colors)), highly detailed photorealistic, hq, 4k, tranding on artstation, shot on leica, raytracing, octane render, 80mm"
+        payload['denoising_strength'] = 0.75
+        payload['steps'] = 40
+        payload['width'] = 768
+        payload['sampler_name'] = "DPM2 Karras"
+        payload['height'] = 768
+        payload['negative_prompt'] = 'blur, unfocus, cropped, ugly'
+        return payload
+
+    @staticmethod
+    def soft_anime_girls(image):
+        """
+        USE DOSMIX WITH EMA ANIME2f
+        :param image:
+        :return:
+        """
+        payload = {}
+        payload['init_images'] = [image]
+        payload['prompt'] = "sharp focus, best quality, masterpiece anime girl portrait concept art, professional, ((soft colors)), highly detailed photorealistic, hq, 4k, shot on leica, 80mm"
+        payload['denoising_strength'] = 0.75
+        payload['steps'] = 30
+        payload['width'] = 768
+        payload['height'] = 768
+        payload['negative_prompt'] = '(realistic:0.7), (low quality, worst quality:1.4), blur, unfocus, cropped, ugly, blurry, weird'
+        return payload
+
 
 
 im = NFTImage()
-print(im.generate_with_ai())
+
+for i in range(0, 100):
+    im.generate_with_ai()
 
 # Stable Diffusion API не может раскодировать base64 изображение и харкается
 # {'detail': 'Invalid encoded image'}
